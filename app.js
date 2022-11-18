72567259aab6874ec47574bbd727d6edbf62e574
e.omelchenko <omel4encko.evgenya@yandex.ru>
2022-11-19 02:00:27 +0300

    fix: обработка несуществующей ссылки

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use("/", users);
 app.use("/", cards);
+app.use((req, res) => {
+  res.status(404).send({ message: "Страница не найдена" });
+});
 
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
